fa799f1801037f4b29bfb6aa75fab34365b960dc
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-22 14:26:42 -0600

    finished model

--- GameOfThronesIOS5/GameOfThronesIOS5/Models/Character.swift
+++ GameOfThronesIOS5/GameOfThronesIOS5/Models/Character.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct TopLevelDictionary: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case characterID = "id"
+        case firstName
+        case lastName
+        case characterTitle = "title"
+        case characterFamily = "family"
+        case characterImage = "imageUrl"
+    }
+    let characterID: Int
+    let firstName: String
+    let lastName: String
+    let characterTitle: String
+    let characterFamily: String
+    let characterImage: String
+}
