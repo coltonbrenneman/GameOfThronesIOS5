69a129ffca46322396b76d9cf57d405dbc3bafca
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-22 14:49:47 -0600

    finished storyboard. added the viewControllers adn subclassed properly

---
+++ GameOfThronesIOS5/GameOfThronesIOS5/Controllers/ViewControllers/CharacterDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterDetailViewController.swift
+//  GameOfThronesIOS5
+//
+//  Created by Colton Brenneman on 6/22/23.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+} // End of class
